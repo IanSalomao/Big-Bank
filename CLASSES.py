f6dc6de2ad21fdafde1aa47950abd334a2791928
Kaua Felipe <nascimento200408@gmail.com>
2022-03-11 20:04:32 -0300

    Método "extrato()" adicionado

--- CLASSES.py
+++ CLASSES.py
@@ -4,19 +4,22 @@ class usuario():
         self.n_conta = I_numero_conta
         self.nome_usuario = I_nome_usuario
         self.__senha = I_senha
-        self.extrato = 0
+        self.saldo = 0
         self.debito = 0
 
     def deposito(self, valor):
-        usuario.extrato += valor
+        self.saldo += valor
 
     def saque(self, valor):
-        if usuario.extrato >= valor:
-            usuario.extrato -= valor
+        if self.extrato >= valor:
+            self.extrato -= valor
             return "Saque concluido"
 
         else:
-            return "Você não tem saldo para fazer este saque"
+            return "Você não tem saldo suficiente para fazer este saque"
+
+    def extrato(self):
+            print(f"Titular: {self.nome_usuario}\nNumero da Conta: {self.n_conta}\nSaldo: R${self.saldo:.2f}\n")
 
     def empretimo(self, valor, tempo):
         pass
--- MAIN.py
+++ MAIN.py
@@ -1,5 +1,5 @@
 import CLASSES as cf
 
-
 test = cf.usuario("1234-5","Testando","1234")
-print(test.n_conta)
+test.deposito(500)
+test.extrato()
